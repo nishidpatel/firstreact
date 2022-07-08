0c391f3eca3607e9b0654fba9180ce5bda91eaa1
nishidpatel <nishidpatel71@gmail.com>
2022-07-08 10:30:37 +0530

    8_7_firstreact

--- src/App.js
+++ src/App.js
@@ -2,6 +2,56 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+ let arr = [12,34,35,37,45];
+ let ans = Array , isArray (arr)
+ console.log(ans); 
+
+// ====================================
+
+//  let grid = [25,36,47,29,38,"x"];
+//  console.log(grid [3],grid); 
+
+//   const display = (Name,id) => {
+//     console.log(Name,id);
+// }
+// display ("nishid",10);
+
+//============================== 
+//  const x = 5;
+//  {
+//   const x =10;
+//  } 
+//  console.log(x);
+
+// let x = 5;
+// {
+//   let x = 10;
+// }
+
+// console.log(x);
+
+// ===========================
+  // const x;
+  // x = 5;
+
+  // console.log(x);
+
+  // let x;
+  // x = 5;
+
+  // console.log(x);
+
+  // ======================================
+  // const x = 10;
+  // x = x + 5;
+
+  // console.log(x);
+
+  // let x = 10;
+  // x = x + 5
+
+  // console.log(x); 
   return (
     <div className="App">
       <header className="App-header">
@@ -15,11 +65,11 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React firstreact
         </a>
       </header>
     </div>
   );
-}
+  }
 
 export default App;
