34dfc7d99ef295040e419f63d5466855ae6beef7
nishidpatel <nishidpatel71@gmail.com>
2022-07-12 10:19:20 +0530

    12_7_firstreact

--- src/App.js
+++ src/App.js
@@ -3,47 +3,41 @@ import './App.css';
 
 function App() {
 
-  // let arr = [11,22,33,44,55];
-  // let arr1 = [50,60];
+  // ======array======
 
-  // let newarr = arr.concat(arr1);
-  // console.log(newarr);        //[11, 22, 33, 44, 55, 50, 60]
+    // let arr = [4,5,6,1,2,3];
 
-  // =====================================
+  // let arr1 = [...arr];
+  // console.log(arr1);
+ //=============================
+ 
+//  let arr2 = [11,22];
+//  let ans = [...arr,...arr2];
+//  console.log(ans);      //[4, 5, 6, 1, 2, 3, 11, 22]
 
-  // let arr = [10,20,30,40,50];
-  // let ans = arr.some((a) => a> 20);
+// =================================
 
-  // console.log(ans);     //true
+  // const [f,s,t,fo,fi,si] = arr;
+  // console.log(fi);
 
 
-  // ======================================
+  // ==========obj==============
 
-  // let arr = [13,23,33]
+  // let obj = {id:101 ,name: 'nishid'};
 
-  // console.log(arr.toString(arr));
+  //  let obj1 = {...obj};
+  //  console.log(obj1);     //{id: 101, name: 'nishid'}
 
-  // ========================================
+  // ==============================
 
-  // let arr = [10,20,30,40];
-  // let ans = arr.find((a) => a> 20);
+  // let obj2 = {id: 102 , place: 'kamrej'};
+  // let ans = {...obj, ...obj2};
+  // console.log(ans);     //{id: 102, name: 'nishid', place: 'kamrej'}
 
-  //  console.log(ans);     //30
+  // ==============================
 
-  // =======================================
-
-  // let arr = [14,24,34,44];
-  // arr.splice (2,0,99);
-  // console.log(arr);    //[14, 24, 99, 34, 44]
-
-  // =======================================
-
-  // let obj = {
-  //   id : 101,
-  //   name : 'nishid'
-  // };
-
-  // console.log(obj.id,obj['name']);     //101 'nishid'
+  // let {id,name} = obj;
+  // console.log(name,obj,id);     //{id: 101, name: 'nishid'} 101
 
 
 
