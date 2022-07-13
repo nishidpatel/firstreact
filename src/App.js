752ec55c5feed9c24b3ec84e92d8f8f2e5ef2d43
nishidpatel <nishidpatel71@gmail.com>
2022-07-13 10:48:55 +0530

    13_7_data

--- src/App.js
+++ src/App.js
@@ -3,60 +3,71 @@ import './App.css';
 
 function App() {
 
-  // ======array======
+  let Data=
+  [
+     {
+       id: 101,
+       name: 'Abacavir',
+       quantity: 25,
+       price: 150,
+       expiry: 2022,
+       status: true
+     },
+     {
+       id: 102,
+       name: 'Eltrombopag',
+       quantity: 90,
+       price: 550,
+       expiry: 2021,
+       status: true
+     },
+     {
+       id: 103,
+       name: 'Meloxicam',
+       quantity: 85,
+       price: 450,
+       expiry: 2025,
+       status: false
+     },
+     {
+       id: 104,
+       name: 'Allopurinol',
+       quantity: 50,
+       price: 600,
+       expiry: 2023,
+       status: true
+     },
+     {
+       id: 105,
+       name: 'Phenytoin',
+       quantity: 63,
+       price: 250,
+       expiry: 2021,
+       status: false
+     }
+   ];
 
-    // let arr = [4,5,6,1,2,3];
+   let fdata = Data.filter((v,i)=>v.expiry >=2022 && v.status === true);
+   console.log(fdata);
 
-  // let arr1 = [...arr];
-  // console.log(arr1);
- //=============================
- 
-//  let arr2 = [11,22];
-//  let ans = [...arr,...arr2];
-//  console.log(ans);      //[4, 5, 6, 1, 2, 3, 11, 22]
+   let totalprise = fdata.reduce((acc,v,i)=>acc+v.price,0);
+   console.log(totalprise);
 
-// =================================
-
-  // const [f,s,t,fo,fi,si] = arr;
-  // console.log(fi);
-
-
-  // ==========obj==============
-
-  // let obj = {id:101 ,name: 'nishid'};
-
-  //  let obj1 = {...obj};
-  //  console.log(obj1);     //{id: 101, name: 'nishid'}
-
-  // ==============================
-
-  // let obj2 = {id: 102 , place: 'kamrej'};
-  // let ans = {...obj, ...obj2};
-  // console.log(ans);     //{id: 102, name: 'nishid', place: 'kamrej'}
-
-  // ==============================
-
-  // let {id,name} = obj;
-  // console.log(name,obj,id);     //{id: 101, name: 'nishid'} 101 //
-
+   
 
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React firstreact
-        </a>
-      </header>
+    <div>
+      {
+        Data.map ((v,i) =>{
+          return(
+            <div key={i}>
+            <h1>{v.name}</h1>
+            <h4>{v.price}</h4>
+            </div>
+          )
+        })
+      }
     </div>
   );
   }
