3e29239f5b3c44b10db0036e772591c28d00cb8d
nishidpatel <nishidpatel71@gmail.com>
2022-07-13 16:22:48 +0530

    13_7_2data

--- src/App.js
+++ src/App.js
@@ -5,52 +5,48 @@ function App() {
 
   let Data=
   [
-     {
-       id: 101,
-       name: 'Abacavir',
-       quantity: 25,
-       price: 150,
-       expiry: 2022,
-       status: true
-     },
-     {
-       id: 102,
-       name: 'Eltrombopag',
-       quantity: 90,
-       price: 550,
-       expiry: 2021,
-       status: true
-     },
-     {
-       id: 103,
-       name: 'Meloxicam',
-       quantity: 85,
-       price: 450,
-       expiry: 2025,
-       status: false
-     },
-     {
-       id: 104,
-       name: 'Allopurinol',
-       quantity: 50,
-       price: 600,
-       expiry: 2023,
-       status: true
-     },
-     {
-       id: 105,
-       name: 'Phenytoin',
-       quantity: 63,
-       price: 250,
-       expiry: 2021,
-       status: false
-     }
-   ];
+    {
+      name: "amit",
+      age: 35,
+      salary: 40000,
+      bonus: 1000,
+      status: true
+    },
+    {
+      name: "ajay",
+      age: 25,
+      salary: 38000,
+      bonus: 2000,
+      status: false
+    },
+    {
+      name: "mayur",
+      age: 23,
+      salary: 50000,
+      bonus: 500,
+      status: true
+    },
+    {
+      name: "jay",
+      age: 29,
+      salary: 35000,
+      bonus: 600,
+      status: true
+    },
+    {
+      name: "raj",
+      age: 33,
+      salary: 22000,
+      bonus: 2000,
+      status: true
+    },
+  ]
 
-   let fdata = Data.filter((v,i)=>v.expiry >=2022 && v.status === true);
+
+   let fdata = Data.filter((v,i)=>v.status === true );
    console.log(fdata);
 
-   let totalprise = fdata.reduce((acc,v,i)=>acc+v.price,0);
+   let totalprise = fdata.reduce((acc,v,i)=>acc+v.bonus + v.salary,0);
    console.log(totalprise);
 
    
@@ -63,7 +59,9 @@ function App() {
           return(
             <div key={i}>
             <h1>{v.name}</h1>
-            <h4>{v.price}</h4>
+            <h3>{v.salary}</h3>
+            <h4>{v.bonus}</h4>
+            <h5>{v.bonus + v.salary}</h5>
             </div>
           )
         })
