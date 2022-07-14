c59641f63225d9131669d841dd488188f8d3f13c
nishidpatel <nishidpatel71@gmail.com>
2022-07-14 11:18:06 +0530

    14_7_student_data

--- src/App.js
+++ src/App.js
@@ -3,61 +3,60 @@ import './App.css';
 
 function App() {
   let data =
-  [
-    {
-      grid: 123,
-      name: 'Amit',
-      fees: 50000,
-      attendance: 80
-    },
-    {
-      grid: 456,
-      name: 'Ajay',
-      fees: 70000,
-      attendance: 90
-    },
-    {
-      grid: 856,
-      name: 'Ashish',
-      fees: 30000,
-      attendance: 65
-    },
-    {
-      grid: 762,
-      name: 'Raj',
-      fees: 90000,
-      attendance: 70
-    },
+    [
+      {
+        grid: 123,
+        name: 'Amit',
+        fees: 50000,
+        attendance: 80
+      },
+      {
+        grid: 456,
+        name: 'Ajay',
+        fees: 70000,
+        attendance: 90
+      },
+      {
+        grid: 856,
+        name: 'Ashish',
+        fees: 30000,
+        attendance: 65
+      },
+      {
+        grid: 762,
+        name: 'Raj',
+        fees: 90000,
+        attendance: 70
+      },
 
-  ]
+    ]
 
 
-   let fdata = data.filter((v,i)=>v.attendance < 71 );
-   console.log(fdata);
+  let fdata = data.filter((v, i) => v.attendance < 71);
+  console.log(fdata);
 
-   let totalprise = fdata.reduce((acc,v,i)=> acc + v.fees,0);
-   console.log(totalprise);
+  let totalprice = fdata.reduce((acc, v, i) => acc + v.fees, 0);
+  console.log(totalprice);
 
-   
+
 
 
   return (
     <div>
       {
-        fdata.map ((v,i) =>{
-          return(
+        fdata.map((v, i) => {
+          return (
             <div key={i}>
-            <h1>{v.name}</h1>
-            <h2>{v.grid}</h2>
-            <h3>{v.fees}</h3>
-            
+              <h1>{v.name}</h1>
+              <h2>{v.grid}</h2>
+              <h3>{v.fees}</h3>
             </div>
           )
         })
       }
-      <h3>totalprise :-{totalprise}</h3>
+      <h3>totalprice :-{totalprice}</h3>
     </div>
   );
-  }
+}
 
 export default App;
