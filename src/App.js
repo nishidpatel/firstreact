55357138dc879227e8029a4d1cd81ea34b9eba6b
nishidpatel <nishidpatel71@gmail.com>
2022-08-03 22:14:30 +0530

    3_8_functioncuntry

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React,{Component} from 'react';
-import City from './function/City';
+import React, { Component } from 'react';
+// import ClassBasedDemo from './container/ClassBasedDemo';
+// import Country from './container/Country';
+// import City from './Function/City';
+import Country from './Function/Country';
+
 
 function App(props) {
-    return (
-        <div>
-             <City />
-        </div>
-    );
+  return (
+    <div>
+      {/* <ClassBasedDemo /> */}
+      {/* <Country/> */}
+      {/* <City/> */}
+      <Country/>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/function/Cuntry.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+
+function Country(props) {
+
+    const[CountryName , setCountryName] =useState('india');
+
+    const changeCountry  =  () => {
+         setCountryName ('UK')
+
+       
+    }
+    return (
+        <div>
+            <p>{CountryName}</p>
+            <button onClick ={() => changeCountry()}>changeCountry</button>
+        </div>
+    );
+}
+
+export default Country;
